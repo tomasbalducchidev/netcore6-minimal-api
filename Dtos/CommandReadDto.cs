edfa2faef2d0a7265cc10f57f3ce1562b316100b
Tomas Balducchi <tomasbalducchi@gmail.com>
2024-03-14 08:56:49 -0300

    initial commit

---
+++ Dtos/CommandReadDto.cs
@@ -0,0 +1,15 @@
+namespace SixMinApi.Dtos
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+
+        public string? HowTo { get; set; }
+
+        public string? Platform { get; set; }
+
+        public string? CommandLine { get; set; }
+        
+
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using SixMinApi.Dtos;
+using SixMinApi.Models;
+
+namespace SixMinApi.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+            
+        }
+    }
+}
